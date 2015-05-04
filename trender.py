6b5cfd498376a69c476494808573d0c9b6dd9764
angelbeats23 <dexgriffiths@hotmail.co.uk>
2015-05-04 01:03:38 +0100

    commented the trender.py file

--- trender.py
+++ trender.py
@@ -15,19 +15,24 @@ def main():
         cmd = raw_input("> ")
         if cmd == "status":
             # sends a output to the terminal
-            # if threads aren't functioning "
+            # if controller threads has stopped functioning "Thread has stopped"
+            # if controller thread is running "Database is being written"
             controller.status()
         else:
             print"unknown command"
 
 
 def signal_handler(signal, frame):
+        # the flag indicates to the signal hander that the controller thread object is
+        # ready to close down
         flag = controller.get_flag()
         while not flag:
             pass
+        # closes both threads
         print '\nExit Program Safely'
         sys.exit(0)
 
+# Listens for the Ctrl C command to be executed then closes the application
 signal.signal(signal.SIGINT, signal_handler)
 
 
